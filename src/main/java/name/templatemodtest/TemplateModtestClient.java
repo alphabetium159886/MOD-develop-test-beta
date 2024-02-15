f2f458d10478d751e9cc059401798459c0567d54
alphabetium159886 <toby1920@outlook.com>
2024-02-15 13:11:02 +0800

    Initial commit

---
+++ src/main/java/name/templatemodtest/TemplateModtestClient.java
@@ -0,0 +1,10 @@
+package name.templatemodtest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModtestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
